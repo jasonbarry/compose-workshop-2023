5618c1850e09ebe7cb8ad7527c46d5cb3b92aeaa
Jason Barry <jb@netlify.com>
2023-10-18 15:25:22 -0700

    Add fine-grained cache control

--- netlify/functions/books.ts
+++ netlify/functions/books.ts
@@ -1,3 +1,4 @@
+import { createHash } from 'crypto';
 import { Config } from '@netlify/functions';
 
 import csv from 'csvtojson';
@@ -5,6 +6,21 @@ import csv from 'csvtojson';
 export default async (req: Request, context: Context) => {
   const { id } = context.params;
   console.log(`Looking up ${id || 'all books'}...`);
+
+  const etag = createHash('md5')
+    .update(id || 'all')
+    .digest('hex');
+
+  const headers = {
+    'Cache-Control': 'public, max-age=0, must-revalidate', // Tell browsers to always revalidate
+    'Netlify-CDN-Cache-Control': 'public, max-age=31536000, must-revalidate', // Tell Edge to cache asset for up to a year
+    'Cache-Tag': `books,promotions`,
+    ETag: `"${etag}"`,
+  };
+
+  if (req.headers.get('if-none-match') === etag) {
+    return new Response('Not modified', { status: 304, headers });
+  }
 
   const { origin } = new URL(req.url);
   const response = await fetch(`${origin}/books.csv`);
@@ -14,12 +30,12 @@ export default async (req: Request, context: Context) => {
   if (id) {
     const book = books.find(b => b.id === id);
     if (!book) {
-      return new Response('Not found', { status: 404 });
+      return new Response('Not found', { status: 404, headers });
     }
-    return Response.json(book);
+    return Response.json(book, { headers });
   }
 
-  return Response.json(books);
+  return Response.json(books, { headers });
 };
 
 export const config: Config = {
